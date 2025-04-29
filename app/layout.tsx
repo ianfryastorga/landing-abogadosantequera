8415d864813fce32cbd16631c406acf9266d1716
ianfryastorga <ianfryastorga@gmail.com>
2025-04-28 20:10:49 -0400

    Actualización del componente Layout: se unificaron los scripts de Google Analytics y Google Ads, actualizando el ID de seguimiento y simplificando la implementación. Se eliminaron scripts redundantes para optimizar el rendimiento.

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,38 +67,17 @@ export default function RootLayout({
           {children}
         </LanguageProvider>
         
-        {/* Google Analytics - Colocar tu ID de GA */}
+        {/* Google Analytics y Google Ads unificados */}
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16733289511"
           strategy="afterInteractive"
         />
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script id="google-analytics-ads" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-XXXXXXXXXX');
-          `}
-        </Script>
-        
-        {/* Google Ads - Colocar tu ID de Google Ads */}
-        <Script id="google-ads" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-XXXXXXX');
-          `}
-        </Script>
-        
-        {/* Google Ads Conversion Tracking Script */}
-        <Script id="google-ads-conversion" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-XXXXXXXXXX');
+            gtag('config', 'AW-16733289511');
           `}
         </Script>
       </body>
